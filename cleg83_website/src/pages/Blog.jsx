5cc4e7e4fc0bb8335116917d0b65cc1c6d4d6080
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-21 17:20:06 +0100

    Add some snapping and height properties to blog post container

--- cleg83_website/src/pages/Blog.jsx
+++ cleg83_website/src/pages/Blog.jsx
@@ -2,7 +2,7 @@ import BlogPost from '../../src/pages/components/Blog_Posts';
 
 export default function Blog() {
   return (
-    <div>
+    <div className="h-screen snap-y snap-mandatory">
       <BlogPost file="/posts/favourite_albums.md" />,
       <BlogPost file="/posts/test_post2.md" />,
       <BlogPost file="/posts/test_post3.md" />,
