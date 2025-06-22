54ad32752cd5a2644184ec7ac02b64d188a8a538
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-22 12:18:22 +0100

    Import contact form component

--- cleg83_website/src/pages/Contact.jsx
+++ cleg83_website/src/pages/Contact.jsx
@@ -1,8 +1,15 @@
+import ContactForm from '../../src/pages/components/Contact_Form';
+
 export default function Contact() {
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold text-green-600">Contact Page</h1>
-      <p className="text-green-950">Get in touch with us!</p>
+    <div className="bg-gray-200 min-h-screen">
+      <div className="pt-6">
+        <h1 className="text-3xl font-bold">Any Questions?</h1>
+        <p className="mt-2">Get in touch with us!</p>
+      </div>
+      <div>
+        <ContactForm />
+      </div>
     </div>
   );
 }
