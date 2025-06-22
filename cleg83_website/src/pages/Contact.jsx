4c5d6d2bf846943a4a5be1e348cc0266fdb906e9
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-22 16:33:54 +0100

    Add home page image as background for this page

--- cleg83_website/src/pages/Contact.jsx
+++ cleg83_website/src/pages/Contact.jsx
@@ -2,14 +2,19 @@ import ContactForm from '../../src/pages/components/Contact_Form';
 
 export default function Contact() {
   return (
-    <div className="bg-gray-200 min-h-screen">
-      <div className="pt-6">
-        <h1 className="text-3xl font-bold">Any Questions?</h1>
-        <p className="mt-2">Get in touch with us!</p>
-      </div>
-      <div>
-        <ContactForm />
-      </div>
+    <div className="relative min-h-screen text-black pt-16">
+      <div
+        className="absolute inset-0 z-0 bg-[url(/images/pexels-marek-piwnicki-3907296-20066246.jpg)] bg-cover"
+      />
+        <div className="relative z-10">
+          <div>
+            <h1 className="text-3xl font-bold">Any Questions?</h1>
+            <p className="mt-2">Get in touch with us!</p>
+          </div>
+          <div>
+            <ContactForm />
+          </div>
+        </div>
     </div>
   );
 }
