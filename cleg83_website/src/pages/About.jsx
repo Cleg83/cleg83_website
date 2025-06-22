93790e2afdb639086c2967b0611c5eecf8aaf13a
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-22 16:31:51 +0100

    Add content container to pad content in lince with other pages

--- cleg83_website/src/pages/About.jsx
+++ cleg83_website/src/pages/About.jsx
@@ -1,6 +1,6 @@
 export default function About() {
   return (
-    <div className="w-full p-4">
+    <div className="w-full min-h-screen pt-16">
       <h1 className="text-3xl font-bold text-orange-600">About Page</h1>
       <p className="mt-2 text-orange-950">Welcome to the about page!</p>
     </div>
