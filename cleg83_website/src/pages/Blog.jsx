d344d2ad5394e5e915e74af44735dc9951f24847
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-23 15:30:27 +0100

    Remove unnecessary commas

--- cleg83_website/src/pages/Blog.jsx
+++ cleg83_website/src/pages/Blog.jsx
@@ -3,9 +3,9 @@ import BlogPost from '../../src/pages/components/Blog_Posts';
 export default function Blog() {
   return (
     <div className="h-screen overflow-y-auto no-scrollbar snap-y snap-mandatory">
-      <BlogPost file="/posts/favourite_albums.md" />,
-      <BlogPost file="/posts/test_post2.md" />,
-      <BlogPost file="/posts/test_post3.md" />,
+      <BlogPost file="/posts/favourite_albums.md" />
+      <BlogPost file="/posts/test_post2.md" />
+      <BlogPost file="/posts/test_post3.md" />
       <BlogPost file="/posts/test_post4.md" />
     </div>
   );
