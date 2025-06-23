cc6a3ddfe59c413845436ada7ece364a5c932c4d
Cleg83 <mr.m.cleghorn@gmail.com>
2025-06-23 15:31:13 +0100

    Add framer-motion and experiement with home page and contact page

--- cleg83_website/src/pages/Contact.jsx
+++ cleg83_website/src/pages/Contact.jsx
@@ -1,17 +1,30 @@
+import { motion } from 'framer-motion';
 import ContactForm from '../../src/pages/components/Contact_Form';
 
 export default function Contact() {
   return (
-    <div className="relative min-h-screen text-black pt-16">
+    <div className="relative min-h-screen text-black">
+      <div className="bg-black opacity-50 absolute inset-0 z-1"/>
       <div
         className="absolute inset-0 z-0 bg-[url(/images/pexels-marek-piwnicki-3907296-20066246.jpg)] bg-cover"
       />
-        <div className="relative z-10">
-          <div>
-            <h1 className="text-3xl font-bold">Any Questions?</h1>
-            <p className="mt-2">Get in touch with us!</p>
-          </div>
-          <div>
+        <div className="relative pt-16 z-10 min-h-screen backdrop-blur-sm">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay:0.5, duration: 1 }}
+          >
+            <h1 className="text-4xl font-bold">Any Questions?</h1>
+          </motion.div>
+          <motion.div
+            className="pt-6 pb-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }} 
+          >
+            <p className="text-2xl">Get in touch with us!</p>
+          </motion.div>
+          <div className="pt-8 h-full">
             <ContactForm />
           </div>
         </div>
--- cleg83_website/src/pages/Home.jsx
+++ cleg83_website/src/pages/Home.jsx
@@ -1,12 +1,55 @@
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  visible: {
+    transition: {
+      staggerChildren: 0.75,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 0.85 },
+};
+
 export default function Home() {
   return (
-      <div className="relative min-h-screen text-black pt-16">
-        <div
-          className="absolute inset-0 z-0 bg-[url(/images/pexels-marek-piwnicki-3907296-20066246.jpg)] bg-cover"
-        />
-          <div className="relative z-10">
-            <h1 className="text-4xl">Test</h1>
-          </div>
+    <div className="relative min-h-screen text-black">
+      <div className="absolute inset-0 z-0 bg-[url(/images/pexels-marek-piwnicki-3907296-20066246.jpg)] bg-cover" />
+      <div className="relative pt-16 z-10 min-h-screen">
+        <motion.div
+          className="pt-12"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 2.5, duration: 1 }} 
+        >
+          <h1 className="text-4xl text-center">Welcome</h1>
+        </motion.div>
+        <motion.div
+          className="pt-6"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 3.5, duration: 1 }} 
+        >
+          <p className="text-2xl">Let's see what we can build together!</p>
+        </motion.div>
+        <motion.div
+          className="absolute py-2 w-full left-0 bottom-2 flex flex-row justify-around"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {[1, 2, 3].map((_, idx) => (
+            <motion.div
+              key={idx}
+              className="size-96 bg-black rounded"
+              variants={childVariants}
+              transition={{ duration: 1.5 }}
+            />
+          ))}
+        </motion.div>
       </div>
+    </div>
   );
 }
